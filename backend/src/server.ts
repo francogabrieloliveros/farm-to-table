2add82bfea88afdd7da0dfb194c14e89d11632d1
Lance Perus <lfperus@up.edu.ph>
2026-04-21 11:01:54 +0800

    Initial Commit: initial project dependencies and setup

---
+++ backend/src/server.ts
@@ -0,0 +1,13 @@
+import express, { type Request, type Response } from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, TypeScript Express!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
+
