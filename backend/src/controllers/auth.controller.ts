5979fa40a9c16fbbd077fc42ebb7e96abb2a1a86
Lance Perus <lfperus@up.edu.ph>
2026-04-27 10:24:04 +0800

    feat: add auth controllers

---
+++ backend/src/controllers/auth.controller.ts
@@ -0,0 +1,37 @@
+import type { Request, Response } from 'express';
+import { AuthService } from '../services/auth.service.js';
+
+//    Register a new user
+export const postSignUp = async (req: Request, res: Response) => {
+    const { firstName, lastName, email, password } = req.body;
+
+    // Simple validation
+    if (!firstName || !lastName || !email || !password) {
+        return res.status(400).json({ message: 'Please provide all required fields' });
+    }
+
+    try {
+        const user = await AuthService.registerUser(req.body);
+        res.status(201).json(user);
+    } catch (error: any) {
+        // If user already exists, it's a Bad Request (400)
+        res.status(400).json({ message: error.message });
+    }
+};
+
+// Authenticate user & get token
+export const postLogin = async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({ message: 'Please provide email and password' });
+    }
+
+    try {
+        const user = await AuthService.loginUser(email, password);
+        res.status(200).json(user);
+    } catch (error: any) {
+        // Unauthorized (401) for login failures
+        res.status(401).json({ message: 'Invalid email or password' });
+    }
+};
