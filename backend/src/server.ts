79f1529e0bae4d86e3d176b90e2cdb2944993775
Lance Perus <lfperus@up.edu.ph>
2026-04-27 10:31:02 +0800

    feat: add auth routes in main

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,7 +2,9 @@ import express, { type Request, type Response } from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import { connectDB } from './config/db.js';
+import authRoutes from './routes/auth.routes.js';
 import { errorHandler } from './middlewares/errorHandler.js';
+
 
 
 dotenv.config();
@@ -10,18 +12,21 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Connect to database
+// connect to database
 connectDB()
 
-// Middleware
+// middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
+app.use('/api/auth', authRoutes);
+
 app.get('/', (req: Request, res: Response) => {
   res.send('API is running...');
 });
+
 
 // Error Handler
 app.use(errorHandler);
