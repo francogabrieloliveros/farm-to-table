af097cf11918d26eb42d670a89e95e71507743b0
Lance Perus <lfperus@up.edu.ph>
2026-05-01 11:30:35 +0800

    feat: add user services

---
+++ backend/src/services/user.service.ts
@@ -0,0 +1,25 @@
+import User, { type IUser } from '../models/user.model.js';
+
+export interface UpdateUserData {
+  firstName?: string;
+  middleName?: string;
+  lastName?: string;
+  password?: string;
+}
+
+export const updateUserProfile = async (userId: string, updateData: UpdateUserData): Promise<IUser | null> => {
+    const user = await User.findById(userId);
+
+    if (!user) {
+        throw new Error('User not found');
+    }
+
+    if (updateData.firstName) user.firstName = updateData.firstName;
+    if (updateData.lastName) user.lastName = updateData.lastName;
+    if (updateData.middleName !== undefined) user.middleName = updateData.middleName;
+    if (updateData.password) user.password = updateData.password;
+
+    await user.save();
+
+    return user;
+};
